1df88e9e51b05980b4ed269a3e7b39fd83db323b
Cesar Ausa <cesarausaprog@gmail.com>
2024-08-01 22:43:45 -0300

    starting the project

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import typeOrmConfig from '../config/typeOrm';
+import typeOrmConfig from '../config/typeOrm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
